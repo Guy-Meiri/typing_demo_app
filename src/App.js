311eaa9104441cf2782111118072c5aeceb91509
Guy Meiri <guymeiri312@gmail.com>
2020-08-02 02:02:28 +0300

    basic drawing works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from 'react';
+import styles from './myCssTest.module.css';
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const canvasRef = useRef(null);
+	const contextRef = useRef(null);
+	const [ isDrawing, setIsDrawing ] = useState(false);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		canvas.width = window.innerWidth * 2;
+		canvas.height = window.innerHeight * 2;
+		canvas.style.width = `${window.innerWidth}px`;
+		canvas.style.height = `${window.innerHeight}px`;
+
+		const context = canvas.getContext('2d');
+		context.scale(2, 2);
+		context.font = '50px Arial';
+		context.fillStyle = '#00DADA';
+		context.fillText("noom is a cutie pie, she's the real deal dude!", 300, 300);
+		context.lineCap = 'round';
+		context.strokeStyle = '#1f2f3f';
+		context.lineWidth = 100;
+		context.globalAlpha = 0.3;
+
+		contextRef.current = context;
+	}, []);
+
+	const startDrawing = ({ nativeEvent }) => {
+		setIsDrawing(true);
+		const { offsetX, offsetY } = nativeEvent;
+		contextRef.current.beginPath();
+		contextRef.current.moveTo(offsetX, offsetY);
+	};
+
+	const stopDrawing = () => {
+		contextRef.current.closePath();
+		//some code for accessing pixel values
+		// if (contextRef) {
+		// 	let imgd = contextRef.current.getImageData(100, 100, 10, 10);
+		// 	let pix = imgd.data; // in this case, pix will have an array of length 400, (100 pixels, each with 4 numbers- red, green, blue and alpha)
+		// 	// console.log(pix);
+		// }
+
+		setIsDrawing(false);
+	};
+
+	const draw = ({ nativeEvent }) => {
+		if (isDrawing) {
+			const { offsetX, offsetY } = nativeEvent;
+			contextRef.current.lineTo(offsetX, offsetY);
+			contextRef.current.stroke();
+		}
+	};
+
+	return <canvas onMouseDown={startDrawing} onMouseUp={stopDrawing} onMouseMove={draw} ref={canvasRef} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logo from './logo.svg';
+import styles from './myCssTest.module.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		{/* <div>
+			{' '}
+			my app
+			<textarea
+				rows={20}
+				value={
+					'If you want your app to work offline and load faster, you can change If you want your app to work offline and load faster, you can change'
+				}
+				className={styles.mytextarea}
+			/>
+			
+		</div> */}
+		{/* <img src={logo} className="App-logo" alt="logo" className={styles.myImg} /> */}
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
