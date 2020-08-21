533cb83a8f03352ca25cfc2f6bea95d7d5dcedb1
Guy Meiri <guymeiri312@gmail.com>
2020-08-21 20:14:03 +0300

    Updates

---
+++ precache-manifest.1d696f4d30058129ce84735cd4273270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6609d26eb02ac684aab43751c376442",
+    "url": "/typing_demo_app/index.html"
+  },
+  {
+    "revision": "a3b57132828adef69ae0",
+    "url": "/typing_demo_app/static/css/main.1c2405c7.chunk.css"
+  },
+  {
+    "revision": "c06f6aa26cbaa72587f5",
+    "url": "/typing_demo_app/static/js/2.a158866a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing_demo_app/static/js/2.a158866a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b57132828adef69ae0",
+    "url": "/typing_demo_app/static/js/main.8826d1ab.chunk.js"
+  },
+  {
+    "revision": "47cb1f7d4f66bddad7b1",
+    "url": "/typing_demo_app/static/js/runtime-main.56c83067.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typing_demo_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
